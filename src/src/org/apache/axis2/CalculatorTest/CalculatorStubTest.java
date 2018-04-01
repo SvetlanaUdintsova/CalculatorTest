a70cc2b92540cbb5a491d855b6039979d803546c
svetlana.udintsova <svetlana.udintsova@gmail.com>
2018-04-01 21:07:09 +0300

    Multiply, Add, Subtract methods positive tests added. Divide* methods renamed to testDivide*

--- src/src/org/apache/axis2/CalculatorTest/CalculatorStubTest.java
+++ src/src/org/apache/axis2/CalculatorTest/CalculatorStubTest.java
@@ -1,10 +1,15 @@
 package org.apache.axis2.CalculatorTest;
 
 import org.apache.axis2.AxisFault;
-import org.apache.axis2.CalculatorTest.*;
 import org.apache.axis2.CalculatorTest.CalculatorStub;
+import org.apache.axis2.CalculatorTest.CalculatorStub.Add;
+import org.apache.axis2.CalculatorTest.CalculatorStub.AddResponse;
 import org.apache.axis2.CalculatorTest.CalculatorStub.Divide;
 import org.apache.axis2.CalculatorTest.CalculatorStub.DivideResponse;
+import org.apache.axis2.CalculatorTest.CalculatorStub.Multiply;
+import org.apache.axis2.CalculatorTest.CalculatorStub.MultiplyResponse;
+import org.apache.axis2.CalculatorTest.CalculatorStub.Subtract;
+import org.apache.axis2.CalculatorTest.CalculatorStub.SubtractResponse;
 
 import static org.junit.Assert.*;
 
@@ -13,7 +18,7 @@ import org.junit.Test;
 public class CalculatorStubTest {
 
 	@Test
-	public void Divide_SimpleValues_Calculated() throws Exception {
+	public void testDivide_SimpleValues_Calculated() throws Exception {
 		
 		//Arrange
 		CalculatorStub stub = new CalculatorStub("http://www.dneonline.com/calculator.asmx");		
@@ -29,7 +34,7 @@ public class CalculatorStubTest {
 	}
 	
 	@Test (expected = AxisFault.class)
-	public void Divide_ZeroDenominator_Exception() throws Exception {
+	public void testDivide_ZeroDenominator_Exception() throws Exception {
 		
 		//Arrange
 		CalculatorStub stub = new CalculatorStub("http://www.dneonline.com/calculator.asmx");		
@@ -38,22 +43,55 @@ public class CalculatorStubTest {
 		testData.setIntB(0);
 		
 		//Act
-		DivideResponse divideResponse = stub.divide(testData);
+		stub.divide(testData);
 	}
 
 	@Test
-	public void testAdd() {
-		fail("Not yet implemented");
+	public void testAdd_SimpleValues_Calculated() throws Exception {
+		
+		//Arrange
+		CalculatorStub stub = new CalculatorStub("http://www.dneonline.com/calculator.asmx");		
+		Add testData = new Add();
+		testData.setIntA(10);
+		testData.setIntB(5);
+		
+		//Act
+		AddResponse addResponse = stub.add(testData);
+		
+		//Assert		
+		assertEquals(15, addResponse.getAddResult());
 	}
 
 	@Test
-	public void testMultiply() {
-		fail("Not yet implemented");
+	public void testMultiply_SimpleValues_Calculated() throws Exception {
+		
+		//Arrange
+		CalculatorStub stub = new CalculatorStub("http://www.dneonline.com/calculator.asmx");		
+		Multiply testData = new Multiply();
+		testData.setIntA(10);
+		testData.setIntB(5);
+		
+		//Act
+		MultiplyResponse multiplyResponse = stub.multiply(testData);
+		
+		//Assert		
+		assertEquals(50, multiplyResponse.getMultiplyResult());
 	}
 
 	@Test
-	public void testSubtract() {
-		fail("Not yet implemented");
+	public void testSubtract_SimpleValues_Calculated() throws Exception {
+		
+		//Arrange
+		CalculatorStub stub = new CalculatorStub("http://www.dneonline.com/calculator.asmx");		
+		Subtract testData = new Subtract();
+		testData.setIntA(10);
+		testData.setIntB(5);
+		
+		//Act
+		SubtractResponse subtractResponse = stub.subtract(testData);
+		
+		//Assert		
+		assertEquals(5, subtractResponse.getSubtractResult());
 	}
 
 }
